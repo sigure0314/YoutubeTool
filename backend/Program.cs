8ba6db712a3fad3313834b62b5bcb6e64bab669d
sigure0314 <70961895+sigure0314@users.noreply.github.com>
2025-11-15 00:22:06 +0800

    Disable HTTPS redirection in development

--- backend/Program.cs
+++ backend/Program.cs
@@ -64,8 +64,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
+else
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseCors("Default");
 
